5668c57f189416651057139289aa1eec2c8a89a2
iyyannn <logronio.315483@laspinas.sti.edu.ph>
2025-03-20 01:19:12 +0800

    Update interpreter.py

--- interpreterTP2_04.py
+++ interpreterTP2_04.py
@@ -1,25 +1,39 @@
-class Interpreter:
+# Token types
+INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, EOF = (
+    'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', 'LPAREN', 'RPAREN', 'EOF'
+)
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        """String representation of the class instance."""
+        return f'Token({self.type}, {repr(self.value)})'
+
+    __repr__ = __str__
+
+class Lexer:
     def __init__(self, text):
-        self.text = text
-        self.pos = 0
-        self.current_token = None
-        self.current_char = self.text[self.pos]
+        self.text = text  # Input expression, e.g., "3 + 5"
+        self.pos = 0  # Index in text
+        self.current_char = self.text[self.pos] if self.text else None
 
     def error(self):
-        raise Exception('Invalid syntax')
+        raise Exception('Invalid character')
 
     def advance(self):
+        """Advance the `pos` pointer and set `current_char`."""
         self.pos += 1
-        if self.pos < len(self.text):
-            self.current_char = self.text[self.pos]
-        else:
-            self.current_char = None
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
 
     def skip_whitespace(self):
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
 
     def integer(self):
+        """Return a (multidigit) integer consumed from input."""
         result = ''
         while self.current_char is not None and self.current_char.isdigit():
             result += self.current_char
@@ -27,86 +41,110 @@ class Interpreter:
         return int(result)
 
     def get_next_token(self):
+        """Lexical analyzer - Tokenizes the input expression."""
         while self.current_char is not None:
             if self.current_char.isspace():
                 self.skip_whitespace()
                 continue
 
             if self.current_char.isdigit():
-                return ('INTEGER', self.integer())
+                return Token(INTEGER, self.integer())
 
             if self.current_char == '+':
                 self.advance()
-                return ('PLUS', '+')
-            
+                return Token(PLUS, '+')
+
             if self.current_char == '-':
                 self.advance()
-                return ('MINUS', '-')
-            
+                return Token(MINUS, '-')
+
             if self.current_char == '*':
                 self.advance()
-                return ('MUL', '*')
-            
+                return Token(MUL, '*')
+
             if self.current_char == '/':
                 self.advance()
-                return ('DIV', '/')
+                return Token(DIV, '/')
+
+            if self.current_char == '(':
+                self.advance()
+                return Token(LPAREN, '(')
+
+            if self.current_char == ')':
+                self.advance()
+                return Token(RPAREN, ')')
 
             self.error()
-        
-        return ('EOF', None)
+
+        return Token(EOF, None)
+
+class Interpreter:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise Exception('Invalid syntax')
 
     def eat(self, token_type):
-        if self.current_token[0] == token_type:
-            self.current_token = self.get_next_token()
+        """Check if current token type matches expected, then consume it."""
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
         else:
             self.error()
 
     def factor(self):
+        """factor : INTEGER | LPAREN expr RPAREN"""
         token = self.current_token
-        self.eat('INTEGER')
-        return token[1]
+        if token.type == INTEGER:
+            self.eat(INTEGER)
+            return token.value
+        elif token.type == LPAREN:
+            self.eat(LPAREN)
+            result = self.expr()
+            self.eat(RPAREN)
+            return result
 
     def term(self):
+        """term : factor ((MUL | DIV) factor)*"""
         result = self.factor()
-
-        while self.current_token[0] in ('MUL', 'DIV'):
+        while self.current_token.type in (MUL, DIV):
             token = self.current_token
-            if token[0] == 'MUL':
-                self.eat('MUL')
+            if token.type == MUL:
+                self.eat(MUL)
                 result *= self.factor()
-            elif token[0] == 'DIV':
-                self.eat('DIV')
+            elif token.type == DIV:
+                self.eat(DIV)
                 result /= self.factor()
-        
         return result
 
     def expr(self):
-        self.current_token = self.get_next_token()
+        """expr : term ((PLUS | MINUS) term)*"""
         result = self.term()
-
-        while self.current_token[0] in ('PLUS', 'MINUS'):
+        while self.current_token.type in (PLUS, MINUS):
             token = self.current_token
-            if token[0] == 'PLUS':
-                self.eat('PLUS')
+            if token.type == PLUS:
+                self.eat(PLUS)
                 result += self.term()
-            elif token[0] == 'MINUS':
-                self.eat('MINUS')
+            elif token.type == MINUS:
+                self.eat(MINUS)
                 result -= self.term()
-
         return result
-
 
 def main():
     while True:
         try:
-            text = input('calc> ')
-            if not text:
-                continue
-            interpreter = Interpreter(text)
-            result = interpreter.expr()
-            print(result)
-        except Exception as e:
-            print(f"Error: {e}")
+            text = input('calc> ')  # Use input() for Python 3
+        except EOFError:
+            break
+        if not text:
+            continue
+        lexer = Lexer(text)
+        interpreter = Interpreter(lexer)
+        result = interpreter.expr()
+        print(result)
 
 if __name__ == '__main__':
     main()
+
+    
